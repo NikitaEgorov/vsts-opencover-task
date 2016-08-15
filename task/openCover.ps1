0c33fbf3bc4d955588b75875a0e02710b18c4183
NEgorov <nikitaegorov@gmail.com>
2016-08-16 00:13:20 +0300

    Init commit

---
+++ task/openCover.ps1
@@ -0,0 +1,90 @@
+[CmdletBinding()]
+param(
+	#[string][Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] $opencoverExe,
+	[string] $opencoverExe,
+	# [string] $reportGeneratorExe,
+	[string] $target = "$PWD\NUnit-2.6.4\bin\nunit-console.exe",
+	[string] $targetargs = "e:\TFS\EnterpriseDirectories\trunk\_Output\Server\bin\Debug\EnterpriseDirectories.Domain.Tests.Unit.dll /framework=4.0 /noshadow /nologo /out:TestResult.txt /err:StdErr.txt",
+    [string] $output,
+    [string] $registerUser = "true",
+    [string] $filter = "+[EnterpriseDirectories.BLL]* +[EnterpriseDirectories.Domain]* -[*Tests.Unit]*",
+    [string] $excludebyfile = "*\*Generated.cs"
+    # [string] $reportTypes
+)
+
+Write-Output "Open Cover Task Version: 0.1.0"
+Write-Output "OpenCover path: $opencoverExe"
+# Write-Output "ReportGenerator path: $reportGeneratorExe"
+Write-Output "Target: $target"
+Write-Output "Output: $output"
+Write-Output "RegisterUser: $registerUser"
+# Write-Output "reportTypes: $reportTypes"
+
+[bool]$registerUser = $registerUser -eq 'true'
+
+if([string]::IsNullOrEmpty($opencoverExe)){
+	$opencoverExe = "$PWD\opencover.4.6.519\OpenCover.Console.exe"
+}
+if([string]::IsNullOrEmpty($reportGeneratorExe)){
+	$reportGeneratorExe = "ReportGenerator_2.4.5.0\bin\ReportGenerator.exe"
+}
+
+
+if (!(Test-Path $opencoverExe))
+{
+    Write-Error "File '$opencoverExe' not found."
+    return
+}
+
+if (!(Test-Path $target))
+{
+    Write-Error "File '$target' not found."
+    return
+}
+
+
+$exeArgs = @()
+
+$exeArgs+="-target:""$target""" 
+
+$exeArgs+='-targetargs:"' + $targetargs + '"'
+
+if($registerUser) {
+	$exeArgs+="-register:user"
+}
+
+if(![string]::IsNullOrEmpty($excludebyfile)) {
+	$exeArgs+="-excludebyfile:""$excludebyfile"""
+}
+
+if(![string]::IsNullOrEmpty($filter)) {
+	$exeArgs+="-filter:""$filter"""
+}
+
+$exeArgs+="-hideskipped:All"
+$exeArgs+="-skipautoprops"
+$exeArgs+="-returntargetcode"
+$exeArgs+="-output:$PWD\coverage-output.xml"
+
+[string] $exeArgsAsString = [system.String]::Join(" ", $exeArgs)
+
+Write-Host "args: $exeArgsAsString"
+Write-Host "opencoverExe: $opencoverExe"
+
+$pinfo = New-Object System.Diagnostics.ProcessStartInfo
+$pinfo.FileName = $opencoverExe
+$pinfo.RedirectStandardError = $true
+$pinfo.RedirectStandardOutput = $true
+$pinfo.UseShellExecute = $false
+$pinfo.Arguments = $exeArgsAsString
+
+$p = New-Object System.Diagnostics.Process
+$p.StartInfo = $pinfo
+$p.Start() | Out-Null
+$p.WaitForExit()
+$stdout = $p.StandardOutput.ReadToEnd()
+$stderr = $p.StandardError.ReadToEnd()
+
+Write-Host "stdout: $stdout"
+Write-Host "stderr: $stderr"
+Write-Host ("exit code: {0}" -f $p.ExitCode)
