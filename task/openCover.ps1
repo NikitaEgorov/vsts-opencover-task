5ce388819cd119d844b8e097d9ab31a5fafeb1b4
NEgorov <nikitaegorov@gmail.com>
2016-08-16 00:49:59 +0300

    targets

--- task/openCover.ps1
+++ task/openCover.ps1
@@ -2,33 +2,39 @@
 param(
 	#[string][Parameter(Mandatory=$true)][ValidateNotNullOrEmpty()] $opencoverExe,
 	[string] $opencoverExe,
-	# [string] $reportGeneratorExe,
-	[string] $target = "$PWD\NUnit-2.6.4\bin\nunit-console.exe",
-	[string] $targetargs = "Domain.Tests.Unit.dll /framework=4.0 /noshadow /nologo /out:TestResult.txt /err:StdErr.txt",
+	[string] $target,
+	[string] $targetargs,
     [string] $output,
-    [string] $registerUser = "true",
-    [string] $filter = "+[BLL]* +[Domain]* -[*Tests.Unit]*",
-    [string] $excludebyfile = "*\*Generated.cs"
-    # [string] $reportTypes
+    [string] $registerUser,
+    [string] $filter,
+    [string] $excludebyfile,
+	[string] $applyDefaultNUnitArgs
 )
 
 Write-Output "Open Cover Task Version: 0.1.0"
 Write-Output "OpenCover path: $opencoverExe"
-# Write-Output "ReportGenerator path: $reportGeneratorExe"
 Write-Output "Target: $target"
 Write-Output "Output: $output"
 Write-Output "RegisterUser: $registerUser"
-# Write-Output "reportTypes: $reportTypes"
+
+$filter = $filter -replace "\r?\n", " " 
+$excludebyfile = $excludebyfile -replace "\r?\n", " " 
+$targetargs = $targetargs -replace "\r?\n", " " 
 
 [bool]$registerUser = $registerUser -eq 'true'
+[bool]$applyDefaultNUnitArgs = $applyDefaultNUnitArgs -eq 'true'
 
 if([string]::IsNullOrEmpty($opencoverExe)){
 	$opencoverExe = "$PWD\opencover.4.6.519\OpenCover.Console.exe"
 }
-if([string]::IsNullOrEmpty($reportGeneratorExe)){
-	$reportGeneratorExe = "ReportGenerator_2.4.5.0\bin\ReportGenerator.exe"
+
+if([string]::IsNullOrEmpty($target)){
+	$target = "$PWD\NUnit-2.6.4\bin\nunit-console.exe"
 }
 
+if($applyDefaultNUnitArgs) {
+	$targetargs = $targetargs + " /framework=4.0 /noshadow /nologo"
+}
 
 if (!(Test-Path $opencoverExe))
 {
@@ -48,6 +54,8 @@ $exeArgs = @()
 $exeArgs+="-target:""$target""" 
 
 $exeArgs+='-targetargs:"' + $targetargs + '"'
+
+
 
 if($registerUser) {
 	$exeArgs+="-register:user"
